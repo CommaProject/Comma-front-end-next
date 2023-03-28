d00e27abf87e4ae963ab7db253cae1ce4c84a6d0
kim-limit <rlawogks2468@naver.com>
2023-03-28 15:08:57 +0900

    CMA-36 [feat]: signup state 추 가

--- src/stores/actions.ts
+++ src/stores/actions.ts
@@ -1,11 +1,18 @@
 import { atom } from 'jotai';
 
-import type { ModalStateType } from './atoms';
-import { modalStateAtom } from './atoms';
+import { ModalStateType, SignUpStateType } from './atoms';
+import { modalStateAtom, signUpStateAtom } from './atoms';
 
 export const handleModalAtom = atom(
   (get) => get(modalStateAtom),
   (get, set, newModalState: ModalStateType) => {
     set(modalStateAtom, newModalState);
+  },
+);
+
+export const handleSignUpAtom = atom(
+  (get) => get(signUpStateAtom),
+  (get, set, newSignUpState: SignUpStateType) => {
+    set(signUpStateAtom, newSignUpState);
   },
 );
