7226d4cd77b29a11e2634e0b1cc7aeda01cffe19
kim-limit <rlawogks2468@naver.com>
2023-06-17 19:55:17 +0900

    CMA-50 [feat]: oauth link 작성

--- src/components/pages/login/sns-login-box/SnsLoginBox.tsx
+++ src/components/pages/login/sns-login-box/SnsLoginBox.tsx
@@ -1,14 +1,20 @@
+import Link from 'next/link';
 import * as style from './SnsLoginBox.style';
 
 export const SnsLoginBox = () => (
   <style.Wrapper>
     <style.Text>간편 로그인</style.Text>
     <style.Box>
-      {/* TODO : Oauth2 연결 로직 구현 필요 */}
-      <style.KakaoIcon onClick={() => void 0} />
-      <style.NaverIcon onClick={() => void 0} />
+      <Link href="http://43.201.188.96:8080/oauth2/authorization/kakao">
+        <style.KakaoIcon onClick={() => void 0} />
+      </Link>
+      <Link href="http://43.201.188.96:8080/oauth2/authorization/naver">
+        <style.NaverIcon onClick={() => void 0} />
+      </Link>
       <style.InstagramIcon onClick={() => void 0} />
-      <style.GoogleIcon onClick={() => void 0} />
+      <Link href="http://43.201.188.96:8080/oauth2/authorization/google">
+        <style.GoogleIcon onClick={() => void 0} />
+      </Link>
     </style.Box>
   </style.Wrapper>
 );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import LoginTemplate from '@/components/template/login';
-import { useRouter } from 'next/router';
-import axios from 'axios';
+
+import { useLogIn } from '@/hooks/useLogIn';
 
 export async function getStaticProps() {
   return {
@@ -12,43 +12,29 @@ export async function getStaticProps() {
 }
 
 const Login = () => {
-  const router = useRouter();
-
-  const [enteredInputId, setEnteredInputId] = useState('');
-  const [enteredInputPw, setEnteredInputPw] = useState('');
+  const {
+    enteredInputId,
+    enteredInputPw,
+    handleInputId,
+    handleInputPw,
+    handleLogIn,
+  } = useLogIn();
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-  const onChangeInputId = (event: ChangeEvent<HTMLInputElement>): void =>
-    setEnteredInputId(event.target.value);
-  const onChangeInputPw = (event: ChangeEvent<HTMLInputElement>): void =>
-    setEnteredInputPw(event.target.value);
   const onClickIsPasswordVisible = () => {
     setIsPasswordVisible(!isPasswordVisible);
-  };
-  /** 로그인 로직 API 구조만 짜놓았습니다. */
-  const onClickLoginLogic = () => {
-    // TODO : 백엔드 로그인 URI 기입 필요
-    axios
-      .post('/', {
-        id: enteredInputId,
-        password: enteredInputPw,
-      })
-      .then((res) => {
-        // TODO : API SPEC 나온후 수정 필요
-        router.push('/');
-      });
   };
 
   return (
     <LoginTemplate
       inputId={enteredInputId}
       setIsPasswordVisible={setIsPasswordVisible}
-      onChangeInputId={onChangeInputId}
+      onChangeInputId={handleInputId}
       inputPw={enteredInputPw}
-      onChangeInputPw={onChangeInputPw}
+      onChangeInputPw={handleInputPw}
       isPasswordVisible={isPasswordVisible}
       onClickIsPasswordVisible={onClickIsPasswordVisible}
-      onClickLoginLogic={onClickLoginLogic}
+      onClickLoginLogic={handleLogIn}
     />
   );
 };
