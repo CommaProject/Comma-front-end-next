4b0bb272f351c7d86a2e7e814edcdb5927382601
Hwanju Lee <hwanju1596@gmail.com>
2023-07-16 09:08:56 +0900

    fest: Get Redirection Url Cookie test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,13 @@ const Home = () => {
   const currentURL = router.asPath;
   const REDIRECTION_URL = `http://43.201.239.102:3000/`;
 
+  if (currentURL === REDIRECTION_URL) {
+    // Redirection URL에 도달한 경우 처리 로직 작성
+
+    const CookieResponse = getRedirectionURLCookie();
+    console.log('CookieResponse', CookieResponse);
+  }
+
   useEffect(() => {
     if (currentURL === REDIRECTION_URL) {
       // Redirection URL에 도달한 경우 처리 로직 작성
