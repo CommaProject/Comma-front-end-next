00996ff397078586fb859b39fa916458f1db1faf
Hwanju Lee <hwanju1596@gmail.com>
2023-07-16 09:25:34 +0900

    test: Get Redirection Url Cookie test 1. url 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { Button } from '../components/template/login/LoginTemplate.style';
 
 const Home = () => {
   const router = useRouter();
-  const currentURL = router.asPath;
+  const currentURL = `http://43.201.239.102:3000${router.asPath}`;
   const REDIRECTION_URL = `http://43.201.239.102:3000/`;
   console.log('currentURL', currentURL);
   if (currentURL === REDIRECTION_URL) {
