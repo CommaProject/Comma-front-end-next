172dad7c3b039c4809ea5c79a57f50846a9a4ccf
Hwanju Lee <hwanju1596@gmail.com>
2023-08-12 09:08:44 +0900

    style: HorizontalAlbumWithIcon Component

---
+++ src/components/pages/quest/horizontal-album-with-icon/HorizontalAlbumWithIcon.tsx
@@ -0,0 +1,42 @@
+import { HorizontalAlbum } from '@/components/common/album/horizontal-album';
+import { HorizontalAlbumProps } from '@/components/common/album/horizontal-album/HorizontalAlbum';
+import React from 'react';
+import * as style from './HorizontalAlbumWithIcon.style';
+
+interface HorizontalAlbumWithIconProps extends HorizontalAlbumProps {
+  // eslint-disable-next-line no-unused-vars
+  onClickPlusButton: (
+    event: React.MouseEvent<SVGSVGElement, MouseEvent>,
+  ) => void;
+  onClickLikeButton: (
+    event: React.MouseEvent<SVGSVGElement, MouseEvent>,
+  ) => void;
+  isLike: boolean;
+}
+
+const HorizontalAlbumWithIcon = ({
+  imgUrl,
+  songName,
+  singerName,
+  onClick,
+  timer,
+  onClickPlusButton,
+  onClickLikeButton,
+  isLike,
+}: HorizontalAlbumWithIconProps) => (
+  <style.Wrapper>
+    <HorizontalAlbum
+      imgUrl={imgUrl}
+      singerName={singerName}
+      songName={songName}
+      onClick={onClick}
+      timer={timer}
+    />
+    <style.ButtonContainer>
+      <style.addPlayListButton onClick={onClickPlusButton} />
+      <style.likeButton isLike={isLike} onClick={onClickLikeButton} />
+    </style.ButtonContainer>
+  </style.Wrapper>
+);
+
+export default HorizontalAlbumWithIcon;
