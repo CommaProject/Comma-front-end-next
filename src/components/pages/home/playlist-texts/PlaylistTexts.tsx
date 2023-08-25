1a7ebde1bca9678a27583ac7418c7c7a03ca60f9
nayeonLeee <sh05060@naver.com>
2023-08-25 13:01:25 +0900

    빌드 오류 일부 수정2

--- src/components/pages/home/playlist-texts/PlaylistTexts.tsx
+++ src/components/pages/home/playlist-texts/PlaylistTexts.tsx
@@ -14,31 +14,29 @@ export const PlaylistTexts = ({
   isEditMode,
   playlist,
   playTime
-}: PlaylistTextsProps ) => {
-  return (
-    <style.Wrapper 
-    isEditMode={isEditMode} 
-    isPlaylistSelected={isPlaylistSelected}>
-      <style.Text
-        isFontBoldElseRegular
-        isFontNotoElsePre={false}
-        isFontsize15Else12
-      />
-      
-      <style.Text
-        isFontBoldElseRegular
-        isFontNotoElsePre
-        isFontsize15Else12
-      >
-        {playlist.playlistTitle}
-      </style.Text>
-      <style.Text
-        isFontBoldElseRegular={false}
-        isFontNotoElsePre={false}
-        isFontsize15Else12={false}
-      >
-        {playlist.trackCount} songs /{playTime > 360000 ? `${Math.floor((playTime / (1000 * 60)) % 60)}h` : ''} {playTime > 60000 ? `${Math.floor((playTime / (1000 * 60)) % 60)}m` : ''} {Math.floor((playTime / 1000 ) % 60) }s
-      </style.Text>
-    </style.Wrapper>
-  );
-};
+}: PlaylistTextsProps ) => (
+  <style.Wrapper 
+  isEditMode={isEditMode} 
+  isPlaylistSelected={isPlaylistSelected}>
+  <style.Text
+    isFontBoldElseRegular
+    isFontNotoElsePre={false}
+    isFontsize15Else12
+  />
+  
+  <style.Text
+    isFontBoldElseRegular
+    isFontNotoElsePre
+    isFontsize15Else12
+  >
+    {playlist.playlistTitle}
+  </style.Text>
+  <style.Text
+    isFontBoldElseRegular={false}
+    isFontNotoElsePre={false}
+    isFontsize15Else12={false}
+  >
+    {playlist.trackCount} songs /{playTime > 360000 ? `${Math.floor((playTime / (1000 * 60)) % 60)}h` : ''} {playTime > 60000 ? `${Math.floor((playTime / (1000 * 60)) % 60)}m` : ''} {Math.floor((playTime / 1000 ) % 60) }s
+  </style.Text>
+</style.Wrapper>
+);
