148fe4398bbdc5c05fb735605f48ad397dbf79f4
Hwanju Lee <hwanju1596@gmail.com>
2023-09-17 16:24:15 +0900

    feat: history기능 추가 및 delete 로직을 낙관적 쿼리처리

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import {
   getCommaUserAsync,
   getSpotifyArtistAsync,
   getTrackAsync,
   getHistoryAsync,
   addHistoryAsync,
+  deleteAllHistoryAsync,
+  deleteHistoryAsync,
 } from '@/apis/search';
-import { SpotifyArtistProps } from '~/src/constants/types/searchTypes';
+import { SpotifyArtistProps, getHistoryProps } from '@/types/searchTypes';
 import { TrackType } from '@/types/trackTypes';
 import { CommaUserType } from '@/types/authTypes';
 
@@ -31,17 +33,28 @@ const getHistory = async () => {
 
   return { isSuccess, result };
 };
-const setHistory = async (history: string) => {
+const addHistory = async (history: string) => {
   const { isSuccess, result } = await addHistoryAsync(history);
 
   return { isSuccess, result };
 };
+const deleteHistory = async (index: number) => {
+  const { isSuccess, result } = await deleteHistoryAsync(index);
 
+  return { isSuccess, result };
+};
+const deleteAllHistory = async () => {
+  const { isSuccess, result } = await deleteAllHistoryAsync();
+
+  return { isSuccess, result };
+};
 export const useSearch = () => {
   const [spotifyArtistData, setSpotifyArtistData] =
     useState<SpotifyArtistProps[]>();
   const [spotifyTrackData, setSpotifyTrackData] = useState<TrackType[]>();
   const [commaUserData, setCommaUserData] = useState<CommaUserType[]>();
+  const queryClient = useQueryClient();
+  const [searchHistory, setSearchHistory] = useState<getHistoryProps[]>();
 
   const { mutate: mutateTrack } = useMutation(['Track'], getSpotifyTrack, {
     onSuccess: (response) => {
@@ -84,7 +97,7 @@ export const useSearch = () => {
 
   const { mutate: mutateSearchHistory } = useMutation(
     ['searchHistory'],
-    setHistory,
+    addHistory,
     {
       onSuccess: (response) => {
         if (response.result.data) {
@@ -97,15 +110,78 @@ export const useSearch = () => {
       },
     },
   );
+  const useMutationDeleteAllHistory = useMutation({
+    mutationFn: deleteAllHistory,
+    onMutate: async () => {
+      // 기존 데이터를 가지고 있다가 실패하면 사용
+      const oldData = queryClient.getQueryData(['searchHistory']);
 
-  const {
-    isLoading,
-    error,
-    data: searchHistory,
-    isFetching,
-  } = useQuery({
+      // API가 성공해서 업데이트 되지 않게
+      await queryClient.cancelQueries({ queryKey: ['searchHistory'] });
+
+      // 성공한다고 가정
+      if (oldData) {
+        const updatedData = {};
+        queryClient.setQueryData(['searchHistory'], updatedData);
+      }
+
+      // 만약 에러나서 롤백 되면 이전 것을 써놓음.
+      return () => queryClient.setQueryData(['searchHistory'], oldData);
+    },
+    onError: (error, variable, rollback) => {
+      if (rollback) rollback();
+      else console.log(error);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['searchHistory']);
+    },
+  });
+
+  const useMutationDeleteHistory = useMutation({
+    mutationFn: deleteHistory,
+    onMutate: async (id: number) => {
+      // 기존 데이터를 가지고 있다가 실패하면 사용
+      const oldData = searchHistory;
+      const oldDataWithAync = queryClient.getQueryData(['searchHistory']);
+
+      // API가 성공해서 업데이트 되지 않게
+      await queryClient.cancelQueries({ queryKey: ['searchHistory'] });
+
+      // 성공한다고 가정
+      if (oldData) {
+        const updatedData = oldData.filter(
+          (item: getHistoryProps) => item.id !== id,
+        );
+        setSearchHistory(updatedData);
+      }
+
+      // 만약 에러나서 롤백 되면 이전 것을 써놓음.
+      return () => {
+        queryClient.setQueryData(['searchHistory'], oldDataWithAync);
+        setSearchHistory(oldData);
+      };
+    },
+    onError: (error, variable, rollback) => {
+      if (rollback) rollback();
+      else console.log(error);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['searchHistory']);
+    },
+  });
+
+  const { isLoading, error, data, isFetching } = useQuery({
     queryKey: ['searchHistory'],
     queryFn: getHistory,
+    onSuccess: (result) => {
+      if (
+        result?.isSuccess &&
+        result.result.data &&
+        !('errors' in result.result.data)
+      ) {
+        setSearchHistory(result.result.data);
+      }
+    },
   });
 
   return {
@@ -117,5 +193,7 @@ export const useSearch = () => {
     mutateTrack,
     mutateCommaUser,
     mutateSearchHistory,
+    useMutationDeleteAllHistory,
+    useMutationDeleteHistory,
   };
 };
--- src/pages/quest/search.tsx
+++ src/pages/quest/search.tsx
@@ -2,36 +2,28 @@ import { SearchTemplate } from '@/components/template/quest/search';
 import { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/router';
 import { useSearch } from '@/hooks/useSearch';
-import { deleteHistoryAsync, deleteAllHistoryAsync } from '@/apis/search';
-import { useQueryClient } from '@tanstack/react-query';
 import { getHistoryProps } from '@/constants/types/searchTypes';
 import { useAtom } from 'jotai';
 import { searchAtom } from '@/stores/atoms';
 
 const Search = () => {
   const router = useRouter();
-  const queryClient = useQueryClient();
   // Music, Artist, CommaUser
   const [searchItems, setSearchItems] = useAtom(searchAtom);
-  const { searchHistory, mutateSearchHistory } = useSearch();
+  const {
+    searchHistory,
+    mutateSearchHistory,
+    useMutationDeleteHistory,
+    useMutationDeleteAllHistory,
+  } = useSearch();
   const [historyTextArray, setHistoryTextArray] = useState<getHistoryProps[]>();
   const handleOnClickDeleteItem = (index: number) => {
-    console.log(index);
-    deleteHistoryAsync(index);
-    // queryClient.invalidateQueries(['searchHistory']); // 수정 낙관적
+    useMutationDeleteHistory.mutate(index);
   };
 
-  // 수정 탐색페이지에 들어올 때로 수정
-  // 삭제, 전체 삭제
   useEffect(() => {
-    if (
-      searchHistory?.isSuccess &&
-      searchHistory.result.data &&
-      !('errors' in searchHistory.result.data)
-    ) {
-      const reaverseSearchHistory = searchHistory.result.data.sort(
-        (a, b) => b.id - a.id,
-      );
+    if (searchHistory !== undefined) {
+      const reaverseSearchHistory = searchHistory.sort((a, b) => b.id - a.id);
       setHistoryTextArray(reaverseSearchHistory);
     }
   }, [searchHistory]);
@@ -74,7 +66,7 @@ const Search = () => {
     window.history.back();
   };
   const handleAllHistoryDelete = () => {
-    deleteAllHistoryAsync();
+    useMutationDeleteAllHistory.mutate();
   };
   return (
     <SearchTemplate
